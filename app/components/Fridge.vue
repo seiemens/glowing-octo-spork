3829f2f97d4c73f8e7a03f78f194035301063843
wadafacc <schlumpftimo14@gmail.com>
2024-01-08 19:29:59 +0100

    FRIDGE TIME

--- app/components/Fridge.vue
+++ app/components/Fridge.vue
@@ -1,17 +1,34 @@
 <template>
   <div id="fridge">
-    <div class="fridge-head">
+    <div class="fridge-head flex-h flex-v">
+      <span class="title">{{ type }} post-its</span>
       <span class="handle"></span>
     </div>
     <div class="divider"></div>
-    <div class="section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
-    <div class="section"></div>
+    <div class="section flex-h">
+      <span class="handle-2"></span>
+      <PostIt v-for="p in posts" :key="p.id"
+        :id="p.id"
+        :title="p.title"
+        :content="p.content"
+        :author="p.author"
+      />
+    </div>
     <div class="bottom"></div>
   </div>
 </template>
 
 <style src="~/styles/fridge.css"></style>
+
+<script setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: true
+  },
+  posts: {
+    type: Array,
+    required: true
+  }
+});
+</script>
--- app/components/PostIt.vue
+++ app/components/PostIt.vue
@@ -1,7 +1,60 @@
 <template>
   <div class="postit">
-
+    <div class="header flex-v flex-h"> {{ title }}</div>
+    {{ content }}
+    <div class="footer">by {{ author }}</div>
   </div>
 </template>
 
-<style src="~/styles/postit.css"></style>
+
+<!-- Only Inline CSS because i had to use reactive properties for the rotation and margin! -->
+<style scoped>
+
+.header {
+  width: 100%;
+  height: 20%;
+  background-color: var(--top);
+
+  font-family: 'Pacifico', cursive;
+  font-size: 2vh;
+  
+  text-overflow: ellipsis;
+}
+
+.footer {
+  position: absolute;
+  bottom: 0;
+  right:1vh;
+}
+
+.postit {
+  position: relative;
+  width: 20vh;
+  height: 20vh;
+
+  text-overflow: ellipsis;
+  /* border: 0.1vh solid black; */
+
+  background-color: var(--base);
+  font-family: 'Gloria Hallelujah', cursive;
+
+  transform: rotateZ(v-bind(rotation));
+  margin: v-bind(margin);
+}
+</style>
+
+
+<script setup>
+const MAX_ROTATION = 20; // in degree
+const MAX_MARGIN = 7; // in vh
+
+const props = defineProps({
+  title: String,
+  content: String,
+  author: String
+});
+
+const rotation = `${(Math.round(Math.random() * MAX_ROTATION * (Math.random() > 0.5 ? -1 : 1)))}deg`;
+const margin = `${Math.round(Math.random() * MAX_MARGIN)}vh`;
+
+</script>
